78cc8524e5a0fa30db9106e5e8218cfb69f03d12
j000 <jarymut+github@gmail.com>
2020-10-16 11:12:56 +0200

    Add missing header

--- common.h
+++ common.h
@@ -1,15 +1,16 @@
+#include <fcntl.h>
+
 #define COMMON_FILE_NAME "common.h"
 
 const int BLOCK_SIZE = 4000000; // rozmiar bloku danych
 
 struct info_struct {
-  pid_t pid;
-  int offset;
-  int size;
+	pid_t pid;
+	int offset;
+	int size;
 };
 
 struct info_msgbuf {
-  long mtype;   
-  struct info_struct info; 
+	long mtype;
+	struct info_struct info;
 };
-
