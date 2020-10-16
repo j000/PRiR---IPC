f9cb51b98f95277cf9ae9ac2a4509682b04e5718
j000 <jarymut+github@gmail.com>
2020-10-16 11:09:20 +0200

    Init

---
+++ common.h
@@ -0,0 +1,15 @@
+#define COMMON_FILE_NAME "common.h"
+
+const int BLOCK_SIZE = 4000000; // rozmiar bloku danych
+
+struct info_struct {
+  pid_t pid;
+  int offset;
+  int size;
+};
+
+struct info_msgbuf {
+  long mtype;   
+  struct info_struct info; 
+};
+
